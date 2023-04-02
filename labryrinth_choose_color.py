50cc70c41bca837fb47e525b76189eb0e0fb277d
Danny McVey <dannybmcvey@gmail.com>
2023-04-01 18:17:15 -0700

    fill color

--- labryrinth_choose_color.py
+++ labryrinth_choose_color.py
@@ -7,9 +7,11 @@ tile_size = 50
 
 # define color options
 color_options = ['black', 'red', 'blue', 'green', 'orange']
-stroke=random.choice(color_options)
 
-def create_labyrinth(grid_size=grid_size,tile_size=tile_size,stroke=stroke):
+stroke=random.choice(color_options)
+fill=random.choice(['black','white'])
+
+def create_labyrinth(grid_size=grid_size,tile_size=tile_size,stroke=stroke,fill=fill):
 
     # create a new SVG drawing
     dwg = svgwrite.Drawing(filename='labyrinth_in_color.svg', size=(grid_size*tile_size, grid_size*tile_size))
@@ -21,14 +23,14 @@ def create_labyrinth(grid_size=grid_size,tile_size=tile_size,stroke=stroke):
             rand_num = random.random()
             if rand_num < 0.5:
                 # draw a white square with a diagonal line in the top-left to bottom-right direction
-                dwg.add(dwg.rect((i*tile_size, j*tile_size), (tile_size, tile_size), fill='white'))
+                dwg.add(dwg.rect((i*tile_size, j*tile_size), (tile_size, tile_size), fill=fill))
                 dwg.add(dwg.line((i*tile_size, j*tile_size), ((i+1)*tile_size, (j+1)*tile_size), stroke=stroke))
             else:
                 # draw a white square with a diagonal line in the top-right to bottom-left direction
-                dwg.add(dwg.rect((i*tile_size, j*tile_size), (tile_size, tile_size), fill='white'))
+                dwg.add(dwg.rect((i*tile_size, j*tile_size), (tile_size, tile_size), fill=fill))
                 dwg.add(dwg.line(((i+1)*tile_size, j*tile_size), (i*tile_size, (j+1)*tile_size), stroke=stroke))
 
     # save the drawing
     dwg.save()
 
-create_labyrinth(stroke='green')
+create_labyrinth(stroke='green',fill='black')
